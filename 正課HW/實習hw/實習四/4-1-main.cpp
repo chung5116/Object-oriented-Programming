b6ae856ec14bb06c2c78e0a34adc70b4668aac94
chung5116 <53108732+chung5116@users.noreply.github.com>
2020-06-20 10:04:11 +0800

    Add files via upload

---
+++ 正課HW/實習hw/實習四/4-1-main.cpp
@@ -0,0 +1,46 @@
+// Name: ±i¥Ã©Ó	B10706009
+// Date: 2020.04.02
+// Last Update: 2020.04.02
+// Problem statement:  Collatz Conjecture
+#include <iostream>
+using namespace std;
+
+int conjecture(int num) {			// calculate cycle
+	int cycle = 1;
+	while (num != 1) {
+		if (num % 2 == 0) {
+			num /= 2;
+		}
+		else {
+			num = num * 3 + 1;
+		}
+		cycle++;
+	}
+	return	cycle;
+}
+
+
+int main() {
+	int num1 = 0;
+	int num2 = 0;
+
+	while (cin >> num1 >> num2) {
+		int maxcycle = 0;
+		if (num2 >= num1) {
+			for (int i = num1; i <= num2; i++) {
+				int cycle = conjecture(i);
+				if (cycle > maxcycle)
+					maxcycle = cycle;
+			}
+		}
+		else {
+			for (int i = num1; i >= num2; i--) {
+				int cycle = conjecture(i);
+				if (cycle > maxcycle)
+					maxcycle = cycle;
+			}
+		}
+		cout << num1 << " " << num2 << " " << maxcycle << endl;
+	}
+    return 0;
+}
---
+++ 正課HW/實習hw/實習四/4.3-main.cpp
@@ -0,0 +1,149 @@
+// Name: ±i¥Ã©Ó	B10706009
+// Date: 2020.04.02
+// Last Update: 2020.04.02
+// Problem statement:  Student Records
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+
+typedef struct {
+	string firstName;
+	string lastName;
+	string phone;
+}StRec;
+
+void print(vector< StRec>& arrSt);
+void insert(vector< StRec>& arrSt, string firstName, string lastName, string  phone);
+void search(vector< StRec>& arrSt, string firstName, string lastName, string  phone);
+void deleteSt(vector< StRec>& arrSt, string firstName, string lastName, string  phone);
+
+int main() {
+	string command;
+	vector< StRec>arrSt;
+	while (cin >> command) {
+		if (command == "insert") {
+			string firstName;
+			string lastName;
+			string phone;
+			cin >> firstName >> lastName >> phone;
+			insert(arrSt, firstName, lastName, phone);
+		}
+
+		else if (command == "print") {
+			if (arrSt.size() == 0) {
+				cout << "Print Error" << endl;
+			}
+			else {
+				print(arrSt);
+			}
+			//cout << command <<endl;
+		}
+
+		else if (command == "delete") {
+			
+				string firstName;
+				string lastName;
+				string phone;
+				cin >> firstName >> lastName >> phone;
+				deleteSt(arrSt, firstName, lastName, phone);
+		}
+
+		else if (command == "search") {
+			string firstName;
+			string lastName;
+			string phone;
+			cin >> firstName >> lastName >> phone;
+			search(arrSt, firstName, lastName, phone);
+		}
+		else {
+			cout << "Input Error" << endl;
+		}
+	}
+
+	return 0;
+}
+
+
+
+void print(vector< StRec>& arrSt) {
+	for (auto& s : arrSt) {
+		cout << s.firstName << " " << s.lastName << " " << s.phone << endl;
+	}
+}
+
+void insert(vector< StRec>& arrSt, string firstName, string lastName, string  phone) {
+	if (firstName.length() > 25 || lastName.length() > 30 || phone.length() > 15) {
+		cout << "Input Error" << endl;
+		return;
+	}
+	for (int i = 0; i < phone.length(); i++) {
+		if (phone[i] > '9' || phone[i] < '0') {
+			cout << "Input Error" << endl;
+			return;
+		}
+	}
+	if (arrSt.size() == 10) {
+		cout << "Insert Error" << endl;
+		return;
+	}
+	for (auto& s : arrSt) {
+		if (s.firstName == firstName 
+			&& s.lastName == lastName
+			&&s.phone == phone) {
+			cout << "Insert Error" << endl;
+			return;
+		}
+	}
+	StRec s;
+	s.firstName = firstName;
+	s.lastName = lastName;
+	s.phone = phone;
+	arrSt.push_back(s);
+
+}
+
+void search(vector< StRec>& arrSt, string firstName, string lastName, string  phone) {
+	if (firstName.length() > 25 || lastName.length() > 30 || phone.length() > 15) {
+		cout << "Input Error" << endl;
+		return;
+	}
+	for (int i = 0; i < phone.length(); i++) {
+		if (phone[i] > '9' || phone[i] < '0') {
+			cout << "Input Error" << endl;
+			return;
+		}
+	}
+	for (int i = 0; i < arrSt.size(); i++) {
+		if (arrSt[i].firstName == firstName
+			&& arrSt[i].lastName == lastName
+			&& arrSt[i].phone == phone) {
+			cout << i << endl;
+			return;
+		}
+	}
+	cout << "Search Error" << endl;
+}
+void deleteSt(vector< StRec>& arrSt, string firstName, string lastName, string  phone) {
+	if (firstName.length() > 25 || lastName.length() > 30 || phone.length() > 15) {
+		cout << "Input Error" << endl;
+		return;
+	}
+	for (int i = 0; i < phone.length(); i++) {
+		if (phone[i] > '9' || phone[i] < '0') {
+			cout << "Input Error" << endl;
+			return;
+		}
+	}
+	vector< StRec>::iterator it;
+	for (it = arrSt.begin(); it != arrSt.end(); it++) {
+		if ((*it).firstName == firstName
+			&& (*it).lastName == lastName
+			&& (*it).phone == phone) {
+			arrSt.erase(it);
+			return;
+		}
+	}
+	cout << "Delete Error" << endl;
+}
